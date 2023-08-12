5a5cc60db8181c5576093125c845902d1288dfc2
Sharook <sk.sharook106@gmail.com>
2023-08-12 12:38:24 +0530

    Ver 1

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,11 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,4,6,isManual,"SUV",5);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = this.getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -17,14 +18,38 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
-        if(newSpeed == 0) {
-            //Stop the car, set gear as 1
-        }
-        //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
+        this.setCurrentSpeed(newSpeed);
+        if(newSpeed > 0 && newSpeed <= 50) {
+            changeGear(1);
             changeSpeed(newSpeed, getCurrentDirection());
+        }// for all other cases, change the gear accordingly
+        else if (newSpeed > 51 && newSpeed <= 100) {
+            changeGear(2);
+            changeSpeed(newSpeed,getCurrentDirection());
         }
+        else if (newSpeed > 101 && newSpeed <= 150) {
+            changeGear(3);
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+        else if (newSpeed > 151 && newSpeed <= 200) {
+            changeGear(4);
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+        else if (newSpeed > 201 && newSpeed <= 250) {
+            changeGear(5);
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+        else if(newSpeed > 250)
+        {
+            changeSpeed(newSpeed, getCurrentDirection());
+            changeGear(6);
+        }
+        else{
+//          Stop the car, set gear as 1
+            this.setCurrentSpeed(0);
+            stop();
+            changeGear(1);
+        }
+
     }
 }
