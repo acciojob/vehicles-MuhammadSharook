ee36026c982dfd2ff974a9e37f73b28781835434
Sharook <sk.sharook106@gmail.com>
2023-08-13 14:36:32 +0530

    Attempt-4

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,23 +5,9 @@ public class Boat implements WaterVehicle{
     private int capacity;
 
     public Boat() {
+
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
 
     public Boat(String name, int capacity) {
         this.name = name;
@@ -30,11 +16,11 @@ public class Boat implements WaterVehicle{
 
     @Override
     public String getVehicleName() {
-        return this.name;
+        return name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return this.capacity;
+        return capacity;
     }
 }
